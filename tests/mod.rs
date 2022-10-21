a0b8e0c7f71ac6489fbd53fb42092e7cd51c3717
Nathaniel McCallum <nathaniel@profian.com>
2022-10-21 08:39:05 +0100

    refactor: clean up tests a bit for readability
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- tests/mod.rs
+++ tests/mod.rs
@@ -72,73 +72,66 @@ stderr: {}
 
 #[test]
 async fn wasi_sync_dir() -> anyhow::Result<()> {
+    const CMD: &str = r#"ls /
+echo 'test' > test
+cat test
+exit
+"#;
+
+    const OUT: &str = r#"
+test
+"#;
+
+    // Set up a temporary directory.
     let tmp = tempdir().context("failed to create a temporary directory")?;
     let dir = File::open(&tmp)
         .map(wasmtime_wasi::sync::Dir::from_std_file)
         .map(wasmtime_wasi::sync::dir::Dir::from_cap_std)
         .map(Box::new)
         .with_context(|| format!("failed to open `{}`", tmp.path().display()))?;
-    let (out, err) = wash(
-        dir,
-        r#"ls /
-echo 'test' > test
-cat test
-exit
-"#,
-    )
-    .context("failed to execute `wash`")?;
 
+    // Run the script and test the output.
+    let (out, err) = wash(dir, CMD).context("failed to execute `wash`")?;
     let out = String::from_utf8_lossy(&out);
     let err = String::from_utf8_lossy(&err);
-    assert_eq!(
-        out,
-        r#"
-test
-"#,
-        "{err}"
-    );
+    assert_eq!(out, OUT, "{err}");
     Ok(())
 }
 
 #[test]
 async fn tmpfs() -> anyhow::Result<()> {
-    let ledger = Ledger::new();
-    let dir = wasmtime_vfs_tmpfs::Builder::from(Arc::clone(&ledger))
-        .add("file", Some(b"file".to_vec()))
-        .await
-        .context("failed to add `/file` to tmpfs")?
-        .add("dir", None)
-        .await
-        .context("failed to add `/dir/` to tmpfs")?
-        .add("dir/a.file", Some(b"file".to_vec()))
-        .await
-        .context("failed to add `/dir/file` to tmpfs")?
-        .add("dir/b.dir", None)
-        .await
-        .context("failed to add `/dir/dir/` to tmpfs")?
-        .build();
-    let (out, err) = wash(
-        dir,
-        r#"ls /
+    const CMD: &str = r#"ls /
 echo 'test' > test
 ls /
 cat test
 ls /dir
 exit
-"#,
-    )
-    .context("failed to execute `wash`")?;
+"#;
 
-    let out = String::from_utf8_lossy(&out);
-    let err = String::from_utf8_lossy(&err);
-    assert_eq!(
-        out,
-        r#"dir file
+    const OUT: &str = r#"dir file
 dir file test
 test
 a.file b.dir
-"#,
-        "{err}"
-    );
+"#;
+
+    let tree = [
+        ("file", Some(b"file".to_vec())),
+        ("dir", None),
+        ("dir/a.file", Some(b"file".to_vec())),
+        ("dir/b.dir", None),
+    ];
+
+    // Construct the tmpfs tree.
+    let mut builder = wasmtime_vfs_tmpfs::Builder::from(Ledger::new());
+    for (path, data) in tree.into_iter() {
+        builder = builder.add(path, data).await.unwrap();
+    }
+    let dir = builder.build();
+
+    // Run the script and test the output.
+    let (out, err) = wash(dir, CMD).context("failed to execute `wash`")?;
+    let out = String::from_utf8_lossy(&out);
+    let err = String::from_utf8_lossy(&err);
+    assert_eq!(out, OUT, "{err}");
     Ok(())
 }
