6898c9278f5138596e197d230d91762abe4a6fe0
Nicholas Farshidmehr <nicholas@profian.com>
2022-10-24 09:12:09 -0400

    chore: add some tests for the ledger
    Signed-off-by: Nicholas Farshidmehr <nicholas@profian.com>

--- ledger/src/lib.rs
+++ ledger/src/lib.rs
@@ -3,6 +3,7 @@ use std::ops::{Deref, Range};
 use std::sync::{Arc, Mutex};
 
 /// A ledger of filesystem devices.
+#[derive(Debug)]
 pub struct Ledger(Mutex<(BTreeSet<u64>, Range<u64>)>);
 
 impl Ledger {
@@ -26,7 +27,25 @@ impl Ledger {
     }
 }
 
+#[test]
+fn ledger_create_device() {
+    let ledger = Ledger::new();
+    let mut set = BTreeSet::new();
+
+    let device_id = {
+        let device_id = ledger.clone().create_device();
+        assert_eq!(device_id.id, 0);
+        assert_eq!(device_id.inodes.lock().unwrap().0.len(), 0);
+        assert_eq!(device_id.devices.0.lock().unwrap().0, set);
+        device_id.id
+    };
+
+    set.insert(device_id);
+    assert_eq!(ledger.0.lock().unwrap().0, set);
+}
+
 /// A filesystem device identifier.
+#[derive(Debug)]
 pub struct DeviceId {
     devices: Arc<Ledger>,
     inodes: Mutex<(BTreeSet<u64>, Range<u64>)>,
@@ -47,11 +66,29 @@ impl Deref for DeviceId {
     }
 }
 
+#[test]
+fn device_id_deref() {
+    let device = Ledger::new().create_device();
+    assert_eq!(**device, 0);
+}
+
 impl Eq for DeviceId {}
 impl PartialEq for DeviceId {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
+}
+
+#[test]
+fn device_id_partial_eq() {
+    let ledger = Ledger::new();
+    let device_0 = ledger.clone().create_device();
+    let device_1 = ledger.create_device();
+    assert_eq!(device_0.id, 0);
+    assert_eq!(device_1.id, 1);
+    assert_eq!(device_0, device_0);
+    assert_ne!(device_0, device_1);
+    assert_eq!(device_1, device_1);
 }
 
 impl DeviceId {
@@ -74,10 +111,33 @@ impl DeviceId {
     }
 }
 
+#[test]
+fn device_id_ledger() {
+    let device = Ledger::new().create_device();
+    let _: Arc<Ledger> = device.ledger();
+}
+
+#[test]
+fn device_id_create_inode() {
+    let device = Ledger::new().create_device();
+    assert_eq!(device.inodes.lock().unwrap().0.len(), 0);
+    let mut set = BTreeSet::new();
+    set.insert(device.clone().create_inode().id);
+    assert_eq!(device.inodes.lock().unwrap().0, set);
+}
+
 /// A filesystem inode identifier.
+#[derive(Debug)]
 pub struct InodeId {
     device: Arc<DeviceId>,
     id: u64,
+}
+
+#[test]
+fn inode_id_debug() {
+    let inode_id = Ledger::new().create_device().create_inode();
+    // TODO: consider testing this more
+    format!("{:?}", inode_id);
 }
 
 impl Drop for InodeId {
@@ -101,9 +161,31 @@ impl PartialEq for InodeId {
     }
 }
 
+#[test]
+fn inode_id_partial_eq() {
+    let ledger = Ledger::new();
+    let device_0 = ledger.clone().create_device();
+    let device_1 = ledger.create_device();
+    let d0i0 = device_0.clone().create_inode();
+    let d0i1 = device_0.create_inode();
+    let d1i0 = device_1.create_inode();
+    assert_eq!((d0i0.device().id, d0i0.id), (0, 0));
+    assert_eq!((d0i1.device().id, d0i1.id), (0, 1));
+    assert_eq!((d1i0.device().id, d1i0.id), (1, 0));
+    assert_eq!(d0i0, d0i0);
+    assert_ne!(d0i1, d0i0);
+    assert_ne!(d1i0, d0i0);
+}
+
 impl InodeId {
     /// Get a reference to the device.
     pub fn device(&self) -> Arc<DeviceId> {
         self.device.clone()
     }
+}
+
+#[test]
+fn inode_id_device() {
+    let device_id = Ledger::new().create_device();
+    assert_eq!(device_id.create_inode().device().id, 0);
 }
